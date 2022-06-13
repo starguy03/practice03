1819921510d7ac67a9494f45a981e68ca3762738
priyanka jamuni <priyanka jamuni@LAPTOP-452IGOAF>
2022-06-13 08:00:32 -0700

    Second commit.

---
+++ Hello/src/Pendrive_142.java
@@ -0,0 +1,15 @@
+
+public class Pendrive_142 {
+int size;
+	
+	Pendrive_142(int size)
+	{
+		this.size=size;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
